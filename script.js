334ff6902575f9ce59725da4b04831cb3366ed0e
sencer2022 <senceralpdogdu@gmail.com>
2022-08-03 14:18:36 +0300

    added clipboard and SpeechSynthesisUtterance

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnTranslate = document.querySelector(".btnTranslate");
 const fromText = document.getElementById("from-text");
 const toText = document.getElementById("to-text");
 const exchange = document.querySelector(".exchange");
+const icons = document.querySelectorAll(".icons");
 
 for(let lang in languages){
     // console.log(lang, languages[lang]);
@@ -37,3 +38,26 @@ exchange.addEventListener("click", () => {
     fromLang.value = toLang.value;
     toLang.value = lang;
 });
+
+for (let icon of icons){
+    icon.addEventListener("click", (element) => {
+        if(element.target.classList.contains("fa-copy")) {
+            if(element.target.id == "from") {
+                navigator.clipboard.writeText(fromText.value)
+            } else {
+                navigator.clipboard.writeText(toText.value)
+            }
+            
+        } else {
+            let utterance;
+            if(element.target.id == "from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = fromLang.value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = toLang.value;
+            }
+            speechSynthesis.speak(utterance);
+        }
+    });
+}
